cafea9c5751e1541d90de2979c609fa764b1594a
Brandon Armand Nyamkimbi <111974934+LeRoiCodes@users.noreply.github.com>
2024-08-22 19:04:42 +0100

    finished card info

--- src/app/(routes)/dashboard/page.jsx
+++ src/app/(routes)/dashboard/page.jsx
@@ -3,6 +3,9 @@
 import { UserButton, useUser } from '@clerk/nextjs'
 import React, { useEffect, useState } from 'react'
 import CardInfo from './_components/CardInfo'
+import { db } from '../../../../utils/dbConfig'
+import { getTableColumns } from 'drizzle-orm'
+import { Budgets } from '../../../../utils/schema'
 // import CardInfo from './_components/CardInfo'
 
 
@@ -10,12 +13,33 @@ const Dashboard = () => {
 
   const {user} = useUser()
 
+  const [budgetList, setBudgetList] = useState([])
+  const [incomeList, setIncomeList] = useState([])
+  const [expenseList, setExpenseList] = useState([])
+
+  useEffect(() => {
+    user && getBudgetList()
+  }, [user])
+
+  const getBudgetList = async () => {
+    const result = await db.select({
+      ...getTableColumns(Budgets),
+      totalSpend: sql`sum(${expenses.amount})`.mapWidth(Number),
+      totalItem: sql`count(${expense.id})`.mapWidth(Number)
+    }).from(Budgets).leftJoin(expenses, eq(Budgets.id, expenses.budgetId)).where(eq(Budgets.createdBy, user?.primaryEmailAddress.emailAddress)).groupBy(Budgets.id).orderBy(desc(Budgets.id))
+
+    setBudgetList(result)
+    getAllExpenses()
+  }
+
+  const getAllExpenses = () => {}
+
   return (
     <div className='p-8'>
       <h2 className='font-bold text-4xl'>Hi, {user?.fullName}</h2>
       <p>Here's what happening with your money. Let's manage your expenses</p>
 
-      <CardInfo />
+      <CardInfo budgetList={} incomeList={} />
     </div>
   )
 }
---
+++ utils/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num
+ * @returns {string}
+ */
+
+
+const formatNumber = (num) => {
+    if(num >= 1e9){
+        return (num/1e9).toFixed(1).replace(/\.0$/,"") + "B";
+    }
+    if(num >= 1e6){
+        return (num/1e6).toFixed(1).replace(/\.0$/,"") + "H";
+    }
+    if(num >= 1e3){
+        return (num/1e3).toFixed(1).replace(/\.0$/,"") + "K";
+    }
+    return num.toString()
+}
+
+export default formatNumber
